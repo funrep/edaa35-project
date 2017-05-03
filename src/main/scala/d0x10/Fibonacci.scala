af84a0bb7166bf0e6d7df1f91d1a4d932bb7cad8
funrep <fuuunrep@gmail.com>
2017-05-03 13:45:48 +0200

    Removed algo abstraction

--- src/main/scala/d0x10/Fibonacci.scala
+++ src/main/scala/d0x10/Fibonacci.scala
@@ -1,6 +1,22 @@
 package d0x10
 
-object Fibonacci extends Algorithm[Int, Int] {
-  override def imperative(input: Int) = ???
-  override def functional(input: Int) = ???
+object Fibonacci extends {
+  def imperative(input: Int): Int = {
+    if (input <= 2)
+      return 1
+
+    var result = 0
+    var prev1 = 0
+    var prev2 = 1
+    var i = 0
+    while (i < input) {
+      val tmp = result
+      result += prev1 + prev2
+      prev1 = prev2
+      prev2 = tmp
+      i += 1
+    }
+    result
+  }
+  def functional(input: Int) = ???
 }

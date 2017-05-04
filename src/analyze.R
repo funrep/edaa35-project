01baa4a754ba6de4a236636af789e6dd0ec96e06
funrep <fuuunrep@gmail.com>
2017-05-04 15:11:09 +0200

    Updated R script

--- src/analyze.R
+++ src/analyze.R
@@ -4,6 +4,12 @@ compile <- function() {
     system("scalac ../Quicksort.scala ../Fibonacci.scala ../Primes.scala ../Main.scala")
 }
 
+toPdf <- function(data, outfile) {
+    pdf(outfile)
+    plot(data, type='l')
+    dev.off()
+}
+
 N <- 600
 
 runImp <- function(algo, input, outfile, start=100) {
@@ -37,3 +43,28 @@ analyze <- function(algo, input, paradigm) {
     c(mean0, confidence)
 }
 
+
+main <- function() {
+    data_quick_imp <- runImp("quicksort", "../data/random.txt", "quick", start=0)
+    data_quick_fun <- runFun("quicksort", "../data/random.txt", "quick", start=0)
+    toPdf(data_quick_imp, "../results/invsägning-imp.pdf")
+    toPdf(data_quick_fun, "../results/insvägning-fun.pdf")
+
+    print("fibonacci")
+    fib_imp <- analyze("fibonacci", "10000", "imp")
+    print(fib_imp[1:3])
+    fib_fun <- analyze("fibonacci", "10000", "fun")
+    print(fib_imp[1:3])
+
+    print("primes")
+    primes_imp <- analyze("primes", "1000", "imp")
+    print(primes_imp[1:3])
+    primes_fun <- analyze("primes", "1000", "fun")
+    print(primes_fun[1:3])
+
+    print("quicksort")
+    quick_imp <- analyze("quicksort", "../data/random.txt", "imp")
+    print(quick_imp[1:3])
+    quick_fun <- analyze("quicksort", "../data/random.txt", "fun")
+    print(quick_fun[1:3])
+}
